cffee1578c58188f150cc1b843bf7823e3608b68
unknown <dmesanggok@gmail.com>
2021-06-21 14:28:42 +0800

    icon

--- app/Http/Controllers/IpaymuResponseController.php
+++ app/Http/Controllers/IpaymuResponseController.php
@@ -14,7 +14,7 @@ class IpaymuResponseController extends Controller
     public function success($email, $uid)
     {
         TopUp::where('dokter', $email)->where('uid', $uid)->update(["status" => 1]);
-        // return view('ipaymu_success');
+        return view('ipaymu_success');
     }
 
     public function successView()
